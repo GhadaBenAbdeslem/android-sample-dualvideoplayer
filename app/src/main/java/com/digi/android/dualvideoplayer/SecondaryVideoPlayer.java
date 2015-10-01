2efb2ceb9e63f9f6d0cb64f242d0048adc734e4c
Tatiana Leon <tatiana.leon@digi.com>
2015-10-01 11:31:56 +0200

    [package] Update the package name in the java code and in the Android manifest.
    Signed-off-by: Tatiana Leon <tatiana.leon@digi.com>

--- app/src/main/java/com/digi/android/dualvideoplayer/SecondaryVideoPlayer.java
+++ app/src/main/java/com/digi/android/dualvideoplayer/SecondaryVideoPlayer.java
@@ -1,4 +1,4 @@
-package com.example.android.dualvideoplayer;
+package com.digi.android.dualvideoplayer;
 
 import android.app.Presentation;
 import android.content.Context;
--- app/src/main/java/com/digi/android/dualvideoplayer/VideoFilter.java
+++ app/src/main/java/com/digi/android/dualvideoplayer/VideoFilter.java
@@ -1,4 +1,4 @@
-package com.example.android.dualvideoplayer;
+package com.digi.android.dualvideoplayer;
 
 import java.io.File;
 import java.io.FilenameFilter;
